c9c6d8bd04384545d2375d43ee5d4b27ac9bd0c7
Umesh Joshi <uj19343@gmail.com>
2025-07-03 18:02:05 +0545

    new app was created named as the authRoute

--- firstProject/authRoute/views.py
+++ firstProject/authRoute/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, "website/login.html")
+
+def signUp(request):
+    return render(request, "website/signup.html")
+def home(request):
+    return render(request, "website/home.html")
