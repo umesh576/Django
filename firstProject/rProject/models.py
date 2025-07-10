9caf8ca665c7928c8d1d74dc1f86fec312ef79aa
Umesh Joshi <uj19343@gmail.com>
2025-07-10 22:31:22 +0545

    new model add and learning the relation of model

--- firstProject/rProject/models.py
+++ firstProject/rProject/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+
+from django.contrib.auth.models import User
+
 # Create your models here.
 class chaiModel(models.Model):
     CHAI_TYPE_CHOICE =[
@@ -15,3 +18,26 @@ class chaiModel(models.Model):
 
     def __str__(self):
         return self.name
+    
+# one two many model
+
+
+class ChaiReviews:
+    CHAI_RATING =[
+        ('1',1),('2',2),('3',3),('4',4),('5',5)
+    ]
+    chai = models.ForeignKey(chaiModel, on_delete=models.CASCADE, related_name='chaiReviews' )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userName' )
+    rating = models.IntegerField(max_length=5, choices=CHAI_RATING,default=1)
+    comment = models.TextField()
+    date= models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.userName} review for {self.chai.name}'
+    
+# many to many relation
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+
+
